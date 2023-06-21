9a34fb125d0321441248fed19f50c59bc540c1ce
AzumattDev <azumattgaming@gmail.com>
2023-06-20 20:14:11 -0400

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Bootstrap;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ManagerDetector;
+
+[HarmonyPatch]
+[BepInPlugin(ModGUID, ModName, ModVersion)]
+public class ManagerDetectorPlugin : BaseUnityPlugin
+{
+    internal const string ModName = "ManagerDetector";
+    internal const string ModVersion = "1.0.0";
+    internal const string Author = "Azumatt";
+    private const string ModGUID = Author + "." + ModName;
+
+    private readonly Harmony _harmony = new(ModGUID);
+
+    public static readonly ManualLogSource ManagerDetectorLogger = BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+    private static List<string> _pieceManagerMods = new();
+    private static List<string> _itemManagerMods = new();
+    private static List<string> _itemDataManagerMods = new();
+    private static List<string> _skillManagerMods = new();
+    private static List<string> _locationManagerMods = new();
+    private static List<string> _creatureManagerMods = new();
+
+    public void Awake()
+    {
+        _harmony.PatchAll();
+    }
+
+    [HarmonyPatch(typeof(ZNet), nameof(ZNet.Awake)), HarmonyPostfix, HarmonyPriority(Priority.Last)]
+    private static void LogBeforeConnect()
+    {
+        Harmony harmony = new(ModGUID);
+        LogTheManagers();
+    }
+
+    private static bool Patched;
+
+    [HarmonyPatch(typeof(FejdStartup), nameof(FejdStartup.Awake)), HarmonyPostfix, HarmonyPriority(Priority.Last)]
+    private static void DoPatch()
+    {
+        ManagerDetectorLogger.LogWarning($"Checking for managers.{Environment.NewLine}");
+        if (Patched) return;
+        Patched = true;
+        Harmony harmony = new(ModGUID);
+        LogTheManagers();
+    }
+
+    private static bool HasManager(Assembly assembly, string namespaceName, string className)
+    {
+        return GetManagerType(assembly, namespaceName, className) != null;
+    }
+
+    private static Type GetManagerType(Assembly assembly, string namespaceName, string className)
+    {
+        foreach (var type in assembly.GetTypes())
+        {
+            if (type.Namespace == namespaceName && type.Name == className)
+            {
+                return type;
+            }
+        }
+
+        return null;
+    }
+
+    private static void CheckManagers(Assembly assembly, PluginInfo info)
+    {
+        if (HasManager(assembly, "PieceManager", "BuildPiece"))
+            _pieceManagerMods.Add($"{info.Metadata.Name} [{info.Metadata.GUID} {info.Metadata.Version}]");
+
+        if (HasManager(assembly, "ItemManager", "Item"))
+            _itemManagerMods.Add($"{info.Metadata.Name} [{info.Metadata.GUID} {info.Metadata.Version}]");
+
+        if (HasManager(assembly, "ItemDataManager", "ItemInfo"))
+            _itemDataManagerMods.Add($"{info.Metadata.Name} [{info.Metadata.GUID} {info.Metadata.Version}]");
+
+        if (HasManager(assembly, "SkillManager", "Skill"))
+            _skillManagerMods.Add($"{info.Metadata.Name} [{info.Metadata.GUID} {info.Metadata.Version}]");
+
+        if (HasManager(assembly, "LocationManager", "Location"))
+            _locationManagerMods.Add($"{info.Metadata.Name} [{info.Metadata.GUID} {info.Metadata.Version}]");
+
+        if (HasManager(assembly, "CreatureManager", "Creature"))
+            _creatureManagerMods.Add($"{info.Metadata.Name} [{info.Metadata.GUID} {info.Metadata.Version}]");
+    }
+
+    private static void LogTheManagers()
+    {
+        _pieceManagerMods.Clear();
+        _itemManagerMods.Clear();
+        _itemDataManagerMods.Clear();
+        _skillManagerMods.Clear();
+        _locationManagerMods.Clear();
+        _creatureManagerMods.Clear();
+
+        foreach (var info in Chainloader.PluginInfos.Values)
+        {
+            var assembly = info.Instance.GetType().Assembly;
+            CheckManagers(assembly, info);
+        }
+
+        // Print all mods with managers
+        if (_pieceManagerMods.Count > 0)
+        {
+            if (Application.platform == RuntimePlatform.WindowsPlayer)
+            {
+                ConsoleManager.SetConsoleColor(ConsoleColor.DarkGreen);
+                ConsoleManager.StandardOutStream.WriteLine($"[Debug  :{ModName}] The following mods have PieceManager:");
+                foreach (var mod in _pieceManagerMods)
+                {
+                    ConsoleManager.StandardOutStream.WriteLine($"[Debug  :{ModName}] {mod}");
+                }
+
+                ConsoleManager.SetConsoleColor(ConsoleColor.White);
+            }
+        }
+
+        if (_itemManagerMods.Count > 0)
+        {
+            ConsoleManager.SetConsoleColor(ConsoleColor.DarkYellow);
+            ConsoleManager.StandardOutStream.WriteLine($"{Environment.NewLine}[Debug  :{ModName}] The following mods have ItemManager:");
+            foreach (var mod in _itemManagerMods)
+            {
+                ConsoleManager.StandardOutStream.WriteLine($"[Debug  :{ModName}] {mod}");
+            }
+
+            ConsoleManager.SetConsoleColor(ConsoleColor.White);
+        }
+
+        if (_itemDataManagerMods.Count > 0)
+        {
+            ConsoleManager.SetConsoleColor(ConsoleColor.Blue);
+            ConsoleManager.StandardOutStream.WriteLine($"{Environment.NewLine}[Debug  :{ModName}] The following mods have ItemDataManager:");
+            foreach (var mod in _itemDataManagerMods)
+            {
+                ConsoleManager.StandardOutStream.WriteLine($"[Debug  :{ModName}] {mod}");
+            }
+
+            ConsoleManager.SetConsoleColor(ConsoleColor.White);
+        }
+
+        if (_skillManagerMods.Count > 0)
+        {
+            ConsoleManager.SetConsoleColor(ConsoleColor.Cyan);
+            ConsoleManager.StandardOutStream.WriteLine($"{Environment.NewLine}[Debug  :{ModName}] The following mods have SkillManager:");
+            foreach (var mod in _skillManagerMods)
+            {
+                ConsoleManager.StandardOutStream.WriteLine($"[Debug  :{ModName}] {mod}");
+            }
+
+            ConsoleManager.SetConsoleColor(ConsoleColor.White);
+        }
+
+        if (_locationManagerMods.Count > 0)
+        {
+            ConsoleManager.SetConsoleColor(ConsoleColor.DarkCyan);
+            ConsoleManager.StandardOutStream.WriteLine($"{Environment.NewLine}[Debug  :{ModName}] The following mods have LocationManager:");
+            foreach (var mod in _locationManagerMods)
+            {
+                ConsoleManager.StandardOutStream.WriteLine($"[Debug  :{ModName}] {mod}");
+            }
+
+            ConsoleManager.SetConsoleColor(ConsoleColor.White);
+        }
+
+        if (_creatureManagerMods.Count > 0)
+        {
+            ConsoleManager.SetConsoleColor(ConsoleColor.DarkMagenta);
+            ConsoleManager.StandardOutStream.WriteLine($"{Environment.NewLine}[Debug  :{ModName}] The following mods have CreatureManager:");
+            foreach (var mod in _creatureManagerMods)
+            {
+                ConsoleManager.StandardOutStream.WriteLine($"[Debug  :{ModName}] {mod}");
+            }
+
+            ConsoleManager.SetConsoleColor(ConsoleColor.White);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,86 @@
+# Description
+
+## ManagerDetector
+
+A debugging developer tool.
+
+## This mod is meant to log which mods you have installed that use one of the managers from Blaxxun or me. The log should appear by the time you are at the main menu.
+
+Currently it checks for the following managers:
+
+- PieceManager
+- ItemManager
+- ItemDataManager
+- SkillManager
+- LocationManager
+- CreatureManager
+
+---
+
+### Thank you to ALo for helping test and for the lovely example image below.
+
+
+# FAQ
+
+- What is a "Manager"?
+    - This is an internal tool that some mods use to quickly and easily add new content to the game. It is not a
+      requirement to use a manager some people choose to use Jotunn or code things themselves, but it is a very useful
+      tool should you want to have a modular solution.
+
+![](https://i.imgur.com/c0b7Jsu.png)
+
+
+<details>
+<summary><b>Installation Instructions</b></summary>
+
+***You must have BepInEx installed correctly! I can not stress this enough.***
+
+### Manual Installation
+
+`Note: (Manual installation is likely how you have to do this on a server, make sure BepInEx is installed on the server correctly)`
+
+1. **Download the latest release of BepInEx.**
+2. **Extract the contents of the zip file to your game's root folder.**
+3. **Download the latest release of ManagerDetector from Thunderstore.io.**
+4. **Extract the contents of the zip file to the `BepInEx/plugins` folder.**
+5. **Launch the game.**
+
+### Installation through r2modman or Thunderstore Mod Manager
+
+1. **Install [r2modman](https://valheim.thunderstore.io/package/ebkr/r2modman/)
+   or [Thunderstore Mod Manager](https://www.overwolf.com/app/Thunderstore-Thunderstore_Mod_Manager).**
+
+   > For r2modman, you can also install it through the Thunderstore site.
+   ![](https://i.imgur.com/s4X4rEs.png "r2modman Download")
+
+   > For Thunderstore Mod Manager, you can also install it through the Overwolf app store
+   ![](https://i.imgur.com/HQLZFp4.png "Thunderstore Mod Manager Download")
+2. **Open the Mod Manager and search for "ManagerDetector" under the Online
+   tab. `Note: You can also search for "Azumatt" to find all my mods.`**
+
+   `The image below shows VikingShip as an example, but it was easier to reuse the image.`
+
+   ![](https://i.imgur.com/5CR5XKu.png)
+
+3. **Click the Download button to install the mod.**
+4. **Launch the game.**
+
+</details>
+
+<br>
+<br>
+
+`Feel free to reach out to me on discord if you need manual download assistance.`
+
+# Author Information
+
+### Azumatt
+
+`DISCORD:` Azumatt#2625
+
+`STEAM:` https://steamcommunity.com/id/azumatt/
+
+For Questions or Comments, find me in the Odin Plus Team Discord or in mine:
+
+[![https://i.imgur.com/XXP6HCU.png](https://i.imgur.com/XXP6HCU.png)](https://discord.gg/Pb6bVMnFb2)
+<a href="https://discord.gg/pdHgy6Bsng"><img src="https://i.imgur.com/Xlcbmm9.png" href="https://discord.gg/pdHgy6Bsng" width="175" height="175"></a>
