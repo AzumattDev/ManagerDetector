6572de50e097436850d68dccce47920ad5e3861f
AzumattDev <azumattgaming@gmail.com>
2023-06-20 23:10:15 -0400

    a little less iffy, and a little more spiffy

--- Plugin.cs
+++ Plugin.cs
@@ -1,189 +1,119 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 using System.Reflection;
 using BepInEx;
 using BepInEx.Bootstrap;
-using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
 using UnityEngine;
 
-namespace ManagerDetector;
-
-[HarmonyPatch]
-[BepInPlugin(ModGUID, ModName, ModVersion)]
-public class ManagerDetectorPlugin : BaseUnityPlugin
+namespace ManagerDetector
 {
-    internal const string ModName = "ManagerDetector";
-    internal const string ModVersion = "1.0.0";
-    internal const string Author = "Azumatt";
-    private const string ModGUID = Author + "." + ModName;
-
-    private readonly Harmony _harmony = new(ModGUID);
-
-    public static readonly ManualLogSource ManagerDetectorLogger = BepInEx.Logging.Logger.CreateLogSource(ModName);
-
-    private static List<string> _pieceManagerMods = new();
-    private static List<string> _itemManagerMods = new();
-    private static List<string> _itemDataManagerMods = new();
-    private static List<string> _skillManagerMods = new();
-    private static List<string> _locationManagerMods = new();
-    private static List<string> _creatureManagerMods = new();
-
-    public void Awake()
+    [HarmonyPatch]
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class ManagerDetectorPlugin : BaseUnityPlugin
     {
-        _harmony.PatchAll();
-    }
+        internal const string ModName = "ManagerDetector";
+        internal const string ModVersion = "1.0.1";
+        internal const string Author = "Azumatt";
+        private const string ModGUID = Author + "." + ModName;
 
-    [HarmonyPatch(typeof(ZNet), nameof(ZNet.Awake)), HarmonyPostfix, HarmonyPriority(Priority.Last)]
-    private static void LogBeforeConnect()
-    {
-        Harmony harmony = new(ModGUID);
-        LogTheManagers();
-    }
+        private static readonly ManualLogSource ManagerDetectorLogger = BepInEx.Logging.Logger.CreateLogSource(ModName);
 
-    private static bool Patched;
+        private readonly Harmony _harmony = new(ModGUID);
 
-    [HarmonyPatch(typeof(FejdStartup), nameof(FejdStartup.Awake)), HarmonyPostfix, HarmonyPriority(Priority.Last)]
-    private static void DoPatch()
-    {
-        ManagerDetectorLogger.LogWarning($"Checking for managers.{Environment.NewLine}");
-        if (Patched) return;
-        Patched = true;
-        Harmony harmony = new(ModGUID);
-        LogTheManagers();
-    }
-
-    private static bool HasManager(Assembly assembly, string namespaceName, string className)
-    {
-        return GetManagerType(assembly, namespaceName, className) != null;
-    }
-
-    private static Type GetManagerType(Assembly assembly, string namespaceName, string className)
-    {
-        foreach (var type in assembly.GetTypes())
+        private static readonly List<ManagerInfo> Managers = new()
         {
-            if (type.Namespace == namespaceName && type.Name == className)
-            {
-                return type;
-            }
+            new ManagerInfo { NamespaceName = "PieceManager", ClassName = "BuildPiece", List = new List<string>(), ConsoleColor = ConsoleColor.DarkGreen },
+            new ManagerInfo { NamespaceName = "ItemManager", ClassName = "Item", List = new List<string>(), ConsoleColor = ConsoleColor.DarkYellow },
+            new ManagerInfo { NamespaceName = "ItemDataManager", ClassName = "ItemInfo", List = new List<string>(), ConsoleColor = ConsoleColor.Blue },
+            new ManagerInfo { NamespaceName = "SkillManager", ClassName = "Skill", List = new List<string>(), ConsoleColor = ConsoleColor.Cyan },
+            new ManagerInfo { NamespaceName = "LocationManager", ClassName = "Location", List = new List<string>(), ConsoleColor = ConsoleColor.DarkCyan },
+            new ManagerInfo { NamespaceName = "CreatureManager", ClassName = "Creature", List = new List<string>(), ConsoleColor = ConsoleColor.DarkMagenta },
+        };
+
+        public void Awake()
+        {
+            _harmony.PatchAll();
         }
 
-        return null;
-    }
-
-    private static void CheckManagers(Assembly assembly, PluginInfo info)
-    {
-        if (HasManager(assembly, "PieceManager", "BuildPiece"))
-            _pieceManagerMods.Add($"{info.Metadata.Name} [{info.Metadata.GUID} {info.Metadata.Version}]");
-
-        if (HasManager(assembly, "ItemManager", "Item"))
-            _itemManagerMods.Add($"{info.Metadata.Name} [{info.Metadata.GUID} {info.Metadata.Version}]");
-
-        if (HasManager(assembly, "ItemDataManager", "ItemInfo"))
-            _itemDataManagerMods.Add($"{info.Metadata.Name} [{info.Metadata.GUID} {info.Metadata.Version}]");
-
-        if (HasManager(assembly, "SkillManager", "Skill"))
-            _skillManagerMods.Add($"{info.Metadata.Name} [{info.Metadata.GUID} {info.Metadata.Version}]");
-
-        if (HasManager(assembly, "LocationManager", "Location"))
-            _locationManagerMods.Add($"{info.Metadata.Name} [{info.Metadata.GUID} {info.Metadata.Version}]");
-
-        if (HasManager(assembly, "CreatureManager", "Creature"))
-            _creatureManagerMods.Add($"{info.Metadata.Name} [{info.Metadata.GUID} {info.Metadata.Version}]");
-    }
-
-    private static void LogTheManagers()
-    {
-        _pieceManagerMods.Clear();
-        _itemManagerMods.Clear();
-        _itemDataManagerMods.Clear();
-        _skillManagerMods.Clear();
-        _locationManagerMods.Clear();
-        _creatureManagerMods.Clear();
-
-        foreach (var info in Chainloader.PluginInfos.Values)
+        [HarmonyPatch(typeof(ZNet), nameof(ZNet.Awake)), HarmonyPostfix, HarmonyPriority(Priority.Last)]
+        private static void LogBeforeConnect()
         {
-            var assembly = info.Instance.GetType().Assembly;
-            CheckManagers(assembly, info);
+            LogTheManagers();
         }
 
-        // Print all mods with managers
-        if (_pieceManagerMods.Count > 0)
+        private static bool Patched;
+
+        [HarmonyPatch(typeof(FejdStartup), nameof(FejdStartup.Awake)), HarmonyPostfix, HarmonyPriority(Priority.Last)]
+        private static void DoPatch()
         {
-            if (Application.platform == RuntimePlatform.WindowsPlayer)
+            ManagerDetectorLogger.LogWarning($"Checking for managers.");
+            if (Patched) return;
+            Patched = true;
+            LogTheManagers();
+        }
+
+        private static Type GetManagerType(Assembly assembly, string namespaceName, string className)
+        {
+            foreach (var type in assembly.GetTypes())
             {
-                ConsoleManager.SetConsoleColor(ConsoleColor.DarkGreen);
-                ConsoleManager.StandardOutStream.WriteLine($"[Debug  :{ModName}] The following mods have PieceManager:");
-                foreach (var mod in _pieceManagerMods)
+                if (type.Namespace == namespaceName && type.Name == className)
                 {
-                    ConsoleManager.StandardOutStream.WriteLine($"[Debug  :{ModName}] {mod}");
+                    return type;
                 }
-
-                ConsoleManager.SetConsoleColor(ConsoleColor.White);
             }
+
+            return null;
         }
 
-        if (_itemManagerMods.Count > 0)
+        private static void CheckManagers(Assembly assembly, PluginInfo info)
         {
-            ConsoleManager.SetConsoleColor(ConsoleColor.DarkYellow);
-            ConsoleManager.StandardOutStream.WriteLine($"{Environment.NewLine}[Debug  :{ModName}] The following mods have ItemManager:");
-            foreach (var mod in _itemManagerMods)
+            foreach (var managerInfo in Managers)
             {
-                ConsoleManager.StandardOutStream.WriteLine($"[Debug  :{ModName}] {mod}");
+                if (GetManagerType(assembly, managerInfo.NamespaceName, managerInfo.ClassName) != null)
+                {
+                    managerInfo.List.Add($"{info.Metadata.Name} [{info.Metadata.GUID} {info.Metadata.Version}]");
+                }
             }
-
-            ConsoleManager.SetConsoleColor(ConsoleColor.White);
         }
 
-        if (_itemDataManagerMods.Count > 0)
+        private static void LogTheManagers()
         {
-            ConsoleManager.SetConsoleColor(ConsoleColor.Blue);
-            ConsoleManager.StandardOutStream.WriteLine($"{Environment.NewLine}[Debug  :{ModName}] The following mods have ItemDataManager:");
-            foreach (var mod in _itemDataManagerMods)
+            foreach (var managerInfo in Managers)
             {
-                ConsoleManager.StandardOutStream.WriteLine($"[Debug  :{ModName}] {mod}");
+                managerInfo.List.Clear();
             }
 
-            ConsoleManager.SetConsoleColor(ConsoleColor.White);
-        }
-
-        if (_skillManagerMods.Count > 0)
-        {
-            ConsoleManager.SetConsoleColor(ConsoleColor.Cyan);
-            ConsoleManager.StandardOutStream.WriteLine($"{Environment.NewLine}[Debug  :{ModName}] The following mods have SkillManager:");
-            foreach (var mod in _skillManagerMods)
+            foreach (var info in Chainloader.PluginInfos.Values)
             {
-                ConsoleManager.StandardOutStream.WriteLine($"[Debug  :{ModName}] {mod}");
+                var assembly = info.Instance.GetType().Assembly;
+                CheckManagers(assembly, info);
             }
 
-            ConsoleManager.SetConsoleColor(ConsoleColor.White);
-        }
-
-        if (_locationManagerMods.Count > 0)
-        {
-            ConsoleManager.SetConsoleColor(ConsoleColor.DarkCyan);
-            ConsoleManager.StandardOutStream.WriteLine($"{Environment.NewLine}[Debug  :{ModName}] The following mods have LocationManager:");
-            foreach (var mod in _locationManagerMods)
+            // Print all mods with managers
+            foreach (var managerInfo in Managers)
             {
-                ConsoleManager.StandardOutStream.WriteLine($"[Debug  :{ModName}] {mod}");
+                if (managerInfo.List.Count > 0)
+                {
+                    ConsoleManager.SetConsoleColor(managerInfo.ConsoleColor);
+                    ConsoleManager.StandardOutStream.WriteLine($"{Environment.NewLine}[Debug  :{ModName}] The following mods have {managerInfo.NamespaceName}:");
+                    foreach (var mod in managerInfo.List)
+                    {
+                        ConsoleManager.StandardOutStream.WriteLine($"[Debug  :{ModName}] {mod}");
+                    }
+
+                    ConsoleManager.SetConsoleColor(ConsoleColor.White);
+                }
             }
-
-            ConsoleManager.SetConsoleColor(ConsoleColor.White);
         }
+    }
 
-        if (_creatureManagerMods.Count > 0)
-        {
-            ConsoleManager.SetConsoleColor(ConsoleColor.DarkMagenta);
-            ConsoleManager.StandardOutStream.WriteLine($"{Environment.NewLine}[Debug  :{ModName}] The following mods have CreatureManager:");
-            foreach (var mod in _creatureManagerMods)
-            {
-                ConsoleManager.StandardOutStream.WriteLine($"[Debug  :{ModName}] {mod}");
-            }
-
-            ConsoleManager.SetConsoleColor(ConsoleColor.White);
-        }
+    public class ManagerInfo
+    {
+        public string NamespaceName { get; set; }
+        public string ClassName { get; set; }
+        public List<string> List { get; set; }
+        public ConsoleColor ConsoleColor { get; set; }
     }
 }
--- Thunderstore/CHANGELOG.md
+++ Thunderstore/CHANGELOG.md
@@ -1,3 +1,4 @@
-| `Version` | `Update Notes`    |
-|-----------|-------------------|
-| 1.0.0     | - Initial Release |
+| `Version` | `Update Notes`                                                                 |
+|-----------|--------------------------------------------------------------------------------|
+| 1.0.1     | - Update some internal code to be a little less iffy, and a little more spiffy |
+| 1.0.0     | - Initial Release                                                              |
